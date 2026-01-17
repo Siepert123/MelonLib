e451fe155616fef27ecb0eae6ad0ba1ab9a46984
Siepert123 <createlegacy69@gmail.com>
2026-01-17 10:07:49 +0100

    bumped to 1.9.0

--- src/main/java/com/melonstudios/melonlib/MelonLib.java
+++ src/main/java/com/melonstudios/melonlib/MelonLib.java
@@ -25,7 +25,7 @@ import org.apache.logging.log4j.Logger;
 public class MelonLib {
     public static final String MODID = "melonlib";
     public static final String NAME = "MelonLib";
-    public static final String VERSION = "1.8.1";
+    public static final String VERSION = "1.9.0";
 
     public static Logger logger;
     public static SimpleNetworkWrapper net;
