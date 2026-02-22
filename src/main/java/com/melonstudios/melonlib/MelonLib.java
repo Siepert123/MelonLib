c12cc692223bca174670b67b1f07c8e0108d6fd0
Siepert123 <createlegacy69@gmail.com>
2026-02-22 18:08:49 +0100

    bump to 1.10.0

--- src/main/java/com/melonstudios/melonlib/MelonLib.java
+++ src/main/java/com/melonstudios/melonlib/MelonLib.java
@@ -29,7 +29,7 @@ import org.apache.logging.log4j.Logger;
 public class MelonLib {
     public static final String MODID = "melonlib";
     public static final String NAME = "MelonLib";
-    public static final String VERSION = "1.9.1";
+    public static final String VERSION = "1.10.0";
 
     public static Logger logger;
     public static SimpleNetworkWrapper net;
