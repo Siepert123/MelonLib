d2f7dd269035cb0f338152b699b179b10207d91f
Siepert123 <createlegacy69@gmail.com>
2026-04-26 20:26:34 +0200

    1.11.2

--- src/main/java/com/melonstudios/melonlib/MelonLib.java
+++ src/main/java/com/melonstudios/melonlib/MelonLib.java
@@ -32,7 +32,7 @@ import org.apache.logging.log4j.Logger;
 public class MelonLib {
     public static final String MODID = "melonlib";
     public static final String NAME = "MelonLib";
-    public static final String VERSION = "1.11.1";
+    public static final String VERSION = "1.11.2";
 
     public static Logger logger;
     public static SimpleNetworkWrapper net;
